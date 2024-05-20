d7d18f34d5b8d83cfad901ce56303d03671afe6e
robberbadeia <robber.badeia@gmail.com>
2024-05-20 13:30:01 +0300

    Task 1:Prevent spammers

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,9 +1,19 @@
 import $ from 'jQuery';
+import _ from 'lodash';
 
 $(document).ready(function () {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button/>');
+  const button = $('<button>Click here to get started</button/>')
+  $('body').append(button);
   $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>'); 
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  $('button').on('click', _.debounce(updateCounter, 500));
 });
+
+let count = 0;
+function updateCounter(){
+  count ++;
+  $('#count').html(`${count} clicks on the button`);
+}
