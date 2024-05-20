831d2eb81a1eb22a0f46d8748a82d73b9029a79c
robberbadeia <robber.badeia@gmail.com>
2024-05-20 13:13:46 +0300

    Task 1: Using jQuery

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,9 @@
+import $ from 'jQuery';
+
+$(document).ready(function () {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button/>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>'); 
+});
