56f0171bfd6254e6df5ea576766383bee8db7b67
robberbadeia <robber.badeia@gmail.com>
2024-06-04 16:20:08 +0300

    Task 3: Uppdate Login styles

--- 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.js
@@ -1,33 +1,67 @@
 import React from "react";
 import { StyleSheet, css } from "aphrodite";
 
+const styles = StyleSheet.create({
+  App_body: {
+    fontSize: "1rem",
+    padding: "2em",
+    height: "45%",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "row",
+    "@media (max-width: 900px)": {
+      display: "flex",
+      flexDirection: "column",
+      flexWrap: "wrap",
+    },
+  },
+  input: {
+    margin: "10px",
+    "@media (max-width: 900px)": {
+      height: "30px",
+      border: "0",
+    },
+  },
+  button: {
+    marginBottom: "10px",
+    marginTop: "10px",
+    width: "50px",
+    "@media (max-width: 900px)": {
+      width: "50px",
+    },
+  },
+});
+
 function Login() {
   return (
     <React.Fragment>
-      <div className={css(styles["App-body"])}>
+      <div className={css(styles.App_body)}>
         <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input className={css(styles.input)} type="email" name="email"></input>
-          <label htmlFor="password">Password:</label>
-          <input className={css(styles.input)} type="password" name="password"></input>
-          <button>OK</button>
+        <form className={css(styles.form)}>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              className={css(styles.input)}
+              type="email"
+              name="email"
+              id="email"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              className={css(styles.input)}
+              type="password"
+              name="password"
+              id="password"
+            ></input>
+          </div>
+          <button className={css(styles.button)}>OK</button>
         </form>
       </div>
     </React.Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  "App-body": {
-    fontSize: "1rem",
-    padding: "2em",
-    height: "45%",
-  },
-
-  input: {
-    margin: "10px",
-  },
-});
 
 export default Login;
--- 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -2,22 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, css } from "aphrodite";
 
-class NotificationItem extends React.PureComponent {
-  render() {
-    const { type, value, html, markAsRead, id } = this.props;
-    return (
-      <>
-        {type && value ? (
-          <li className={type === "default" ? css(styles.default) : css(styles.urgent)} onClick={() => markAsRead(id)} data-notification-type={type}>
-            {value}
-          </li>
-        ) : null}
-        {html ? <li onClick={() => markAsRead(id)} data-urgent className={css(styles.urgent)} dangerouslySetInnerHTML={{ __html: html }}></li> : null}
-      </>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   default: {
     color: "blue",
@@ -26,6 +10,35 @@ const styles = StyleSheet.create({
     color: "red",
   },
 });
+
+class NotificationItem extends React.PureComponent {
+  render() {
+    const { type, value, html, markAsRead, id } = this.props;
+    return (
+      <>
+        {type && value ? (
+          <li
+            className={
+              type === "default" ? css(styles.default) : css(styles.urgent)
+            }
+            onClick={() => markAsRead(id)}
+            data-notification-type={type}
+          >
+            {value}
+          </li>
+        ) : null}
+        {html ? (
+          <li
+            onClick={() => markAsRead(id)}
+            data-urgent
+            className={css(styles.urgent)}
+            dangerouslySetInnerHTML={{ __html: html }}
+          ></li>
+        ) : null}
+      </>
+    );
+  }
+}
 
 NotificationItem.propTypes = {
   type: PropTypes.string.isRequired,
