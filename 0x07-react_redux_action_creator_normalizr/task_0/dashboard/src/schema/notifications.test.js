80639dc36afe84e6c807114ae35c084b6d407aea
robberbadeia <robber.badeia@gmail.com>
2024-06-18 13:34:12 +0300

    Task 0

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.test.js
@@ -0,0 +1,25 @@
+import { getAllNotificationsByUser } from "./notifications";
+
+describe("Notification tests", () => {
+  test("gets all notifications for user with ID 5debd764a7c57c7839d722e9", () => {
+    const result = getAllNotificationsByUser("5debd764a7c57c7839d722e9");
+    const expected = [
+      {
+        guid: "2d8e40be-1c78-4de0-afc9-fcc147afd4d2",
+        isRead: true,
+        type: "urgent",
+        value:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+      },
+      {
+        guid: "280913fe-38dd-4abd-8ab6-acdb4105f922",
+        isRead: false,
+        type: "urgent",
+        value:
+          "Non diam phasellus vestibulum lorem sed risus ultricies. Tellus mauris a diam maecenas sed",
+      },
+    ];
+
+    expect(result).toEqual(expected);
+  });
+});
