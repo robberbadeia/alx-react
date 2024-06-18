42bc1706b1ed0d0ebfee7cf182bfbfa6437bb3e2
robberbadeia <robber.badeia@gmail.com>
2024-06-18 18:11:33 +0300

    Task 1: init commit

---
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -0,0 +1,9 @@
+import * as notificationData from "../../../../notifications.json";
+
+function getAllNotificationsByUser(userId) {
+  return notificationData.default
+    .filter((notification) => notification.author.id === userId)
+    .map((notification) => notification.context);
+}
+
+export { getAllNotificationsByUser };
