2fd5df04bfc88c315cf0e09f8dd669f08446fe08
robberbadeia <robber.badeia@gmail.com>
2024-06-18 13:34:52 +0300

    Task 0

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,7 +1,9 @@
 import * as notificationData from "../../../../notifications.json";
 
-export default function getAllNotificationsByUser(userId) {
-  return notificationData
+function getAllNotificationsByUser(userId) {
+  return notificationData.default
     .filter((notification) => notification.author.id === userId)
     .map((notification) => notification.context);
 }
+
+export { getAllNotificationsByUser };
